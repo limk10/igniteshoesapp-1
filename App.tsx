df5890d83d730c436b48827e92181827a0f5ec68
Matheus Barbosa Lopes <matheus_barbosa5@hotmail.com>
2023-08-03 14:55:41 -0400

    app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from "./src/contexts/CartContext";
+import OneSignal from "react-native-onesignal";
+
+OneSignal.setAppId("910687cc-d3e4-46b6-99aa-31fce4a70709");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
